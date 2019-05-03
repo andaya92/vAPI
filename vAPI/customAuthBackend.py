c3f84b45b0301cba34ffd6a0fb4ac519112ff7a0
godlike <andayac@gmail.com>
2019-05-02 23:14:46 -0700

    make it better

--- vAPI/customAuthBackend.py
+++ vAPI/customAuthBackend.py
@@ -2,19 +2,20 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 
 class EmailBackend(ModelBackend):
-    def authenticate(self, email=None, password=None, **kwargs):
+    def authenticate(self, email=None, username=None, password=None, **kwargs):
         UserModel = get_user_model()
         user = None
-        print(email)
-        # try:
+        print(email, username)
+        try:
         # do user look up by email instead
-        if "@" in email:
-            user = UserModel.objects.filter(email=email)
-        else:
-            user = UserModel.objects.filter(username=email)
-        # except UserModel.DoesNotExist:
-        #     return None
+            if username:
+                user = UserModel.objects.get(username=username)
+            elif email:
+                user = UserModel.objects.get(email=email)
+        except UserModel.DoesNotExist:
+            return None
         if user:
-            if user[0].check_password(password) and self.user_can_authenticate(user[0]):
+            print(user)
+            if user.check_password(password) and self.user_can_authenticate(user):
                 return user
         return None
