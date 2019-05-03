d57745b1b7cd2d779eda59f553e8d5eca246c990
godlike <andayac@gmail.com>
2019-05-02 23:33:08 -0700

    make it better

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+admin.site.register(User)
+admin.site.register(Volunteer)
+admin.site.register(VolunteerProvider)
+admin.site.register(VolunteerEvent)
+admin.site.register(VolunteerPost)
+admin.site.register(VolunteerEventSignUp)
+admin.site.register(EventCity)
+admin.site.register(EventState)
--- tmppy.py
+++ tmppy.py
@@ -3,8 +3,8 @@ import requests
 
 r = requests.post('https://cst-499-volunteer-api.herokuapp.com/home/account/new/',
 					{'account_type' : 'volunteer',
-					'username': 'tester',
-					'email' : 'tt@g.com',
+					'username': 'account_test',
+					'email' : 'tat@g.com',
 					'password' : 'kidskids22',
 					'password_confirm' : 'kidskids22'})
 
