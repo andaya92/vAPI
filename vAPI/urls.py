069c758cd30f2956e66b798a86e0925a63e65454
godlike <andayac@gmail.com>
2019-05-09 15:08:32 -0700

    Environ fixup

--- vAPI/urls.py
+++ vAPI/urls.py
@@ -5,6 +5,6 @@ from django.conf import settings
 
 
 urlpatterns = [
-    path('/', admin.site.urls),
+    path('', admin.site.urls),
     path('home/', include('home.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
