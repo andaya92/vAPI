cde4306918b5b4a8a88c858969e4f8bd657bc46b
godlike <andayac@gmail.com>
2019-05-09 15:02:07 -0700

    Environ fixup

--- vAPI/settings.py
+++ vAPI/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import dj_database_url
-from . import EnvironmentPickler
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,6 +23,7 @@ if 'HEROKU' in os.environ:
     on_heroku = True
     DATABASE_URL = os.environ['DATABASE_URL']
 else:
+    from . import EnvironmentPickler
     env = EnvironmentPickler.load_obj("volunteer_API_env")
     DATABASE_URL = env['DATABASE_URL']
 
