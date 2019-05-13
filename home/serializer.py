1ffe20de9da369a7c5fe280e67de51fb3ed79074
godlike <andayac@gmail.com>
2019-05-13 15:06:31 -0700

    return user id via serializer

--- home/serializer.py
+++ home/serializer.py
@@ -6,7 +6,7 @@ from .models import *
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = get_user_model()
-		fields = ('username', 'email')
+		fields = ('username', 'email', 'id')
 
 class VolunteerSerializer(serializers.ModelSerializer):
 	user = UserSerializer()
